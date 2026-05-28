9a0ecac00b110dc8cd9446c0f182d12b529a8ab7
Izzat <Shixbalayevizzet@gmail.com>
2026-05-28 19:31:45 +0000

    task 1-2

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+İki float ədədini toplayan modul.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """a və b float ədədlərini toplayır və nəticəni float olaraq qaytarır."""
+    return a + b
---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+İki mətni birləşdirən modul.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """str1 və str2 mətnlərini birləşdirir və nəticəni string olaraq qaytarır."""
+    return str1 + str2
