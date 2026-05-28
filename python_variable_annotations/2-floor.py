02b84650ebb92104277c8cc25b1f5af221577723
Izzat <Shixbalayevizzet@gmail.com>
2026-05-28 19:36:19 +0000

    task 2-4

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Kəsr ədədi aşağı yuvarlaqlaşdıran modul.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """n kəsr ədədinin 'floor' (aşağı yuvarlaq) dəyərini qaytarır."""
+    return math.floor(n)
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Kəsr ədədi mətnə çevirən modul.
+"""
+
+
+def to_str(n: float) -> str:
+    """n kəsr ədədini string (mətn) tipinə çevirir."""
+    return str(n)
