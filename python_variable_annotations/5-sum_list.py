9123d9c516f9ab95f2f5f65cca0ea4ef5113e327
Izzat <Shixbalayevizzet@gmail.com>
2026-05-28 19:38:04 +0000

    task5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Siyahıdakı ədədləri toplayan modul.
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Float tipli ədədlərdən ibarət siyahını toplayır və qaytarır."""
+    return sum(input_list)
