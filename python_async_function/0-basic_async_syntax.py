6584ee86dbbd68b3c6d4fa9f56cb214231f3752d
Izzat <Shixbalayevizzet@gmail.com>
2026-05-28 19:20:13 +0000

    task0

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
+"""
+Maksimum gecikmə ilə təsadüfi gözləmə modulu.
+"""
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """0 ilə max_delay arasında təsadüfi bir müddət gözləyir
-
-    və həmin müddəti (saniyəni) geri qaytarır.
-    """
-    # random.uniform() 0 ilə max_delay arasında təsadüfi bir float (kəsr) ədəd seçir
+    """0 ilə max_delay arasında təsadüfi saniyə gözləyir və qaytarır."""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
