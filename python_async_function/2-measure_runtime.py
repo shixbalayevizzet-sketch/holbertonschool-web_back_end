275607cb8938c030d4b2bb82e6225e86c01f11ad
Izzat <Shixbalayevizzet@gmail.com>
2026-05-28 19:24:17 +0000

    task2

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,28 +1,22 @@
-#!/usr/bin/env python3
+kkk#!/usr/bin/env python3
+"""
+Measures the runtime of the wait_n routine.
+"""
 import asyncio
 import time
 
-# Əgər əvvəlki faylınızın adı, məsələn, "fayl_adi.py" idisə, oradan import edirik:
-# (Burada şərti olaraq "script" yazılıb, öz faylınızın adını qeyd edə bilərsiniz)
-from script import wait_n
+# wait_n funksiyasını test sisteminin tələb etdiyi qaydada import edirik
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """wait_n(n, max_delay) funksiyasının ümumi icra vaxtını ölçür
-
-    və orta icra vaxtını (total_time / n) float olaraq qaytarır.
     """
-    # Başlanğıc vaxtı qeyd edirik
+    Measures the total execution time for wait_n(n, max_delay)
+    and returns total_time / n.
+    """
     start_time = time.time()
-
-    # Asinxron wait_n funksiyasını burada işə salırıq
     asyncio.run(wait_n(n, max_delay))
-
-    # Bitiş vaxtını qeyd edirik
     end_time = time.time()
 
-    # Ümumi keçən vaxtı hesablayırıq
     total_time = end_time - start_time
-
-    # n-ə bölərək orta göstəricini qaytarırıq
     return total_time / n
