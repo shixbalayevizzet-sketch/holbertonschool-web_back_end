8a3b551ec7fec5208eb058afb7723e2c2595d802
Izzat <Shixbalayevizzet@example.com>
2026-06-01 15:24:11 +0000

    task2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,7 +3,6 @@
 
 import asyncio
 import time
-from typing import List
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
@@ -12,12 +11,12 @@ async def measure_runtime() -> float:
     """Run async_comprehension 4 times in parallel and measure runtime."""
     start = time.perf_counter()
 
-    await asyncio.gather(
+    await asyncio.gather(*[
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension()
-    )
+    ])
 
     end = time.perf_counter()
     return end - start
