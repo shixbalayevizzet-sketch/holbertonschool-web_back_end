ca01eb767a36b24887cc37cd7d7b50facb3834a4
root <shixbalayevizzet@example.com>
2026-06-10 10:49:36 +0000

    task1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -17,26 +17,21 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Return the cached dataset."""
+        """Cached dataset."""
         if self.__dataset is None:
-            with open(self.DATA_FILE, 'r') as f:
+            with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
 
         return self.__dataset
 
-    def get_page(
-        self,
-        page: int = 1,
-        page_size: int = 10
-    ) -> List[List]:
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Return the appropriate page of the dataset."""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
         start_index, end_index = index_range(page, page_size)
-
         dataset = self.dataset()
 
         if start_index >= len(dataset):
