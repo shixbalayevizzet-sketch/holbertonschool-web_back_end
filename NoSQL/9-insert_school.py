3dacdb4ac4f4f0008048fe68cbb5534f8f8d79b4
Izzat <shixbalayevizzet@example.com>
2026-06-17 14:51:40 +0000

    task9

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Insert a new document in a collection."""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Insert a document and return its _id."""
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
