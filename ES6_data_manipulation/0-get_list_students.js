8b81551fc396fa8fee54403d791a5fa2c5bba7a6
Izzat <shixbalayevizzet@example.com>
2026-07-12 17:30:10 +0000

    task0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+/**
+ * Returns a list of student objects with id, firstName, and location attributes.
+ * @returns {Array<{id: number, firstName: string, location: string}>}
+ */
+function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+}
