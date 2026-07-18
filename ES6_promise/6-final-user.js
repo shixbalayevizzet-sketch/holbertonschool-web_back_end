0f15447160ab6643bfb1f5df348a3b09614cb589
Izzat <shixbalayevizzet@example.com>
2026-07-18 13:18:34 +0000

    task6

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+kimport signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ]).then((results) => {
+    return results.map((result) => {
+      return {
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason,
+      };
+    });
+  });
+}
