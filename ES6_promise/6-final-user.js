7e2f38da91964b65f542656b27dd7d74fa19c889
Izzat <shixbalayevizzet@example.com>
2026-07-18 13:24:42 +0000

    task6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -11,7 +11,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
       value:
         result.status === 'fulfilled'
           ? result.value
-          : result.reason,
+          : result.reason.toString(),
     }))
   );
 }
