293e091339c1f027016182b56cd7af269af1b1b6
Izzat <shixbalayevizzet@example.com>
2026-08-02 18:39:18 +0000

    task0

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a string argument in STDOUT.
+ * @param {string} message - The string to print.
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
